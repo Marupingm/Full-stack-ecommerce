9d58727299d7c178c9c67df781101b9e95bce278
topsy <Malatjimj2@gmail.com>
2024-04-08 00:00:00 +0200

    Mongodb Issues fixed

--- models/Product.ts
+++ models/Product.ts
@@ -73,4 +73,4 @@ productSchema.index({ category: 1, createdAt: -1 });
 
 const Product = mongoose.models.Product || mongoose.model<IProduct>('Product', productSchema);
 
-export default Product; 
+export default Product; // Modified on 2025-02-19 00:50:50
